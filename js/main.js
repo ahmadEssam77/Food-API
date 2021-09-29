2a3359cda2adb79b19ea006241f607864d5a3df2
ahmadEssam77 <ahmad77essam@gmail.com>
2021-09-30 01:05:14 +0200

    Get some APIs and managed them via navbar

---
+++ js/main.js
@@ -0,0 +1,54 @@
+// =============== Initialize API old way ===============
+// =============== Story the data from API to my Array ===============
+let storeData = [];
+
+// =============== Select the links ===============
+let links = document.querySelectorAll(".navbar-nav li a");
+
+// =============== Pass the word that I clicked on it to the getAPI function and change the active class ===============
+for (let i = 0; i < links.length; i++) {
+    links[i].addEventListener("click", function(e) {
+        let foodName = e.target.innerHTML;
+        let theElement = e.target;
+        for (let i = 0; i < links.length; i++) {
+            links[i].classList.remove("active");
+        }
+        theElement.classList.add("active");
+        getAPI(foodName);
+    });
+}
+
+// =============== Get API ===============
+function getAPI(cuisine) {
+    let http = new XMLHttpRequest();
+    http.open("GET", `https://forkify-api.herokuapp.com/api/search?q=${cuisine}`);
+    http.send();
+    http.addEventListener("readystatechange", function () {
+        if (http.readyState == 4 && http.status == 200) {
+            storeData = JSON.parse(http.response).recipes;
+            displayMenu();
+        }
+    });
+}
+
+// =============== Display API
+function displayMenu() {
+    let keeper = ``;
+    for (let i = 0; i < storeData.length; i++) {
+        keeper += `
+                <div class="col-md-3 py-4">
+                    <div class="text-center">
+                        <img src="${storeData[i].image_url}" alt="" class="w-100">
+                        <p>${storeData[i].title}</p>
+                        <button class="btn btn-warning">Details</button>
+                        <button class="btn btn-secondary">Recipes</button>
+                    </div>
+                </div>  
+            `
+    }
+
+    document.getElementById('displayHere').innerHTML = keeper;
+}
+
+// ========== Call at the beginning to show data ============
+getAPI("salad");
